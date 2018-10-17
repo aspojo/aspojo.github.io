0bdaef33c718cc6e1d999c37db144259326a3cc9
aspojo <logbug@aliyun.com>
2018-10-17 10:22:30 +0800

    添加angular1版的angular-ui-bootstrap

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+var app = angular.module('demo', []).controller('DemoCtrl', function($scope) {
+    $scope.modules = [
+        {
+            name: 'ngx-ui-split',
+            desc: 'angular6 split 插件，类似jQuery layout，实现界面上下左右分区',
+            src: 'https://github.com/aspojo/ngx-ui-split',
+            home: 'https://aspojo.github.io/ngx-split/'
+        },
+        {
+            name: 'angular-ui-bootstrap',
+            desc: 'angular bootstrap 插件，支持ie8浏览器',
+            src: 'https://github.com/aspojo/angular-ui-bootstrap',
+            home: './angular-ui-bootstrap/'
+        },
+
+    ];
+
+});
